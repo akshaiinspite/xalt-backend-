802b6c034655b4932e83965e5ce01d228f400e0c
Akshai <akshai.inspite@gmail.com>
2026-07-08 11:13:05 +0530

    fix: force Node.js DNS resolver to use Google DNS for MongoDB Atlas connections

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
+try {
+  dns.setServers(['8.8.8.8', '8.8.4.4', '2001:4860:4860::8888', '2001:4860:4860::8844']);
+} catch (err) {
+  console.warn('Unable to set custom DNS servers, using system defaults:', err.message);
+}
 require('dotenv').config();
 
 const inMemoryDb = require('./utils/inMemoryDb');
